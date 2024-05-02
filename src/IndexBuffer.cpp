6345e5ed4612fd0808931484bcd27b843c4dd871
MJanik167 <mjanik167@gmail.com>
2024-05-02 16:46:30 +0200

    sadas

--- src/IndexBuffer.cpp
+++ src/IndexBuffer.cpp
@@ -8,6 +8,7 @@ IndexBuffer::IndexBuffer(const unsigned int *data, unsigned int count) : m_Count
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW);
 }
 
+/// ughghheurwerjwer
 IndexBuffer::~IndexBuffer()
 {
   glDeleteBuffers(1, &m_RendererID);
